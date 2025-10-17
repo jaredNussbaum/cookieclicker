c26a0291cc9e1f9fb73f3a3a268e9566ce1d5404
jaredNussbaum <74433775+jaredNussbaum@users.noreply.github.com>
2025-10-17 19:58:34 +0000

    step 4, added constant deltatime horsie counter

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,29 @@ counter_label.style.marginTop = "10px";
 counter_label.innerHTML = `${counter} horsies`;
 document.body.appendChild(counter_label);
 
+function update_horsie_label() {
+  counter_label.innerHTML = `${Math.trunc(counter)} horsies`;
+}
+
 button.addEventListener("click", function () {
   counter += 1;
-  counter_label.innerHTML = `${counter} horsies`;
+  update_horsie_label();
 });
 
-setInterval(() => { //constant tick rate
+/*setInterval(() => { //constant tick rate
   counter += 1;
   counter_label.innerHTML = `${counter} horsies`;
-}, 1000);
+}, 1000);*/
+
+let horsie_generation = 0;
+
+let lastTime = performance.now();
+
+function calculate_HPS(currentTime: number) { // HPS = horsies per second
+  const deltaTime = (currentTime - lastTime) / 1000;
+  lastTime = currentTime;
+  counter += horsie_generation * deltaTime;
+  requestAnimationFrame(calculate_HPS);
+  update_horsie_label();
+}
+requestAnimationFrame(calculate_HPS);
